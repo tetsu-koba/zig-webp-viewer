87aa3b5f31dcbe404583a9212752a9beffe8f838
Tetsuyuki Kobayashi <tetsu.koba@gmail.com>
2023-05-12 13:48:51 +0900

    Initial commit

---
+++ src/main.zig
@@ -0,0 +1,96 @@
+const std = @import("std");
+const log = std.log;
+const webp = @import("webp.zig");
+const sdl = @cImport({
+    @cInclude("SDL2/SDL.h");
+});
+
+pub const SCREEN_WIDTH: c_int = 800;
+pub const SCREEN_HEIGHT: c_int = 600;
+pub const RESIZE_INTERVAL_MS: u32 = 100;
+
+pub fn main() anyerror!void {
+    const alc = std.heap.page_allocator;
+    const args = try std.process.argsAlloc(alc);
+    defer std.process.argsFree(alc, args);
+    
+    if (args.len < 2) {
+        std.debug.print("Usage: {s} <image.webp>\n", .{args[0]});
+        std.os.exit(1);
+    }
+
+    //const image_data = try std.fs.readFileAlloc(alc, args[1]);
+    //defer alc.free(image_data);
+    var file1 = try std.fs.cwd().openFile(args[1], .{});
+    defer file1.close();
+    const image_data = try file1.readToEndAlloc(alc, 4 * 1024 * 1024 * 1024);
+    
+    if (sdl.SDL_Init(sdl.SDL_INIT_VIDEO) != 0) {
+        log.err("SDL could not initialize! SDL_Error: {s}", .{sdl.SDL_GetError()});
+        return;
+    }
+    defer sdl.SDL_Quit();
+
+    //const (width, height, pixels) = webp.decodeRGBA(image_data);
+    //defer webp.free(pixels);
+    const image = webp.decodeRGBA(image_data);
+    defer webp.free(image.pixels);
+
+    var window = sdl.SDL_CreateWindow("WebP Display", sdl.SDL_WINDOWPOS_UNDEFINED, sdl.SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, sdl.SDL_WINDOW_RESIZABLE);
+    if (window == null) {
+        log.err("Window could not be created! SDL_Error: {s}", .{sdl.SDL_GetError()});
+        return;
+    }
+    defer sdl.SDL_DestroyWindow(window);
+
+    var renderer = sdl.SDL_CreateRenderer(window, -1, sdl.SDL_RENDERER_ACCELERATED | sdl.SDL_RENDERER_PRESENTVSYNC);
+    if (renderer == null) {
+        log.err("Renderer could not be created! SDL_Error: {s}", .{sdl.SDL_GetError()});
+        return;
+    }
+    defer sdl.SDL_DestroyRenderer(renderer);
+
+    var surface = sdl.SDL_CreateRGBSurfaceFrom(image.pixels.ptr, @intCast(c_int, image.width), @intCast(c_int, image.height), 32, @intCast(c_int, image.width * 4), 0x000000FF, 0x0000FF00, 0x00FF0000, 0xFF000000);
+    
+    if (surface == null) {
+        log.err("Unable to create surface! SDL_Error: {s}", .{sdl.SDL_GetError()});
+        return;
+    }
+    defer sdl.SDL_FreeSurface(surface);
+
+    var texture = sdl.SDL_CreateTextureFromSurface(renderer, surface);
+    if (texture == null) {
+        log.err("Unable to create texture from surface! SDL_Error: {s}", .{sdl.SDL_GetError()});
+        return;
+    }
+    defer sdl.SDL_DestroyTexture(texture);
+
+    var quit = false;
+    var resize_time: u32 = 0;
+    while (!quit) {
+        var event: sdl.SDL_Event = undefined;
+        while (sdl.SDL_PollEvent(&event) != 0) {
+            switch (event.type) {
+                sdl.SDL_QUIT => quit = true,
+                sdl.SDL_WINDOWEVENT => |windowEvent| {
+                    //switch (windowEvent.window.event) {
+                    switch (windowEvent) {
+                        sdl.SDL_WINDOWEVENT_RESIZED => resize_time = sdl.SDL_GetTicks(),
+                        else => {},
+                    }
+                },
+                else => {},
+            }
+        }
+        if (sdl.SDL_GetTicks() - resize_time > RESIZE_INTERVAL_MS) {
+            var w: c_int = 0;
+            var h: c_int = 0;
+            sdl.SDL_GetWindowSize(window, &w, &h);
+            var render_quad = sdl.SDL_Rect { .x = 0, .y = 0, .w = w, .h = h };
+            _ = sdl.SDL_RenderClear(renderer);
+            _ = sdl.SDL_RenderCopy(renderer, texture, null, &render_quad);
+            sdl.SDL_RenderPresent(renderer);
+            resize_time = 0;
+        }
+    }
+}
---
+++ src/webp.zig
@@ -0,0 +1,42 @@
+const std = @import("std");
+const webp = @cImport({
+    @cInclude("webp/decode.h");
+});
+pub const Decoder = webp.WebPIDecoder;
+pub const DecoderOptions = webp.WebPDecoderConfig;
+pub const DecoderBuffer = webp.WebPDecBuffer;
+pub const DecoderMode = webp.WEBP_CSP_MODE;
+pub const ImageFormat = webp.WebPInputFileFormat;
+
+extern fn WebPGetInfo(data: [*]const u8, data_size: usize, width: *usize, height: *usize) u32;
+extern fn WebPDecodeRGBA(data: [*]const u8, data_size: usize, width: *usize, height: *usize) [*]u8;
+extern fn WebPFree(pointer: *u8) void;
+
+pub const ImageInfo = struct {
+    width: usize,
+    height: usize,
+};
+
+pub const ImageData = struct {
+    width: usize,
+    height: usize,
+    pixels: []u8,
+};
+
+pub fn getInfo(data: []const u8) ImageInfo {
+    var width: usize = 0;
+    var height: usize = 0;
+    _ = WebPGetInfo(data.ptr, data.len, &width, &height);
+    return ImageInfo{ .width = width, .height = height };
+}
+
+pub fn decodeRGBA(data: []const u8) ImageData {
+    var width: usize = 0;
+    var height: usize = 0;
+    const pixels = WebPDecodeRGBA(data.ptr, data.len, &width, &height);
+    return ImageData{ .width = width, .height = height, .pixels = pixels[0..width * height * 4] };
+}
+
+pub fn free(pointer: []u8) void {
+    WebPFree(@ptrCast(*u8, pointer.ptr));
+}
