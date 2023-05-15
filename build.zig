280c8fb47c16959f834527082647a1f0631e09cb
小林哲之 <koba@koba-mac-2014.local>
2023-05-15 10:14:16 +0900

    Conditional build for macos

--- build.zig
+++ build.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const builtin = @import("builtin");
 
 // Although this function looks imperative, note that its job is to
 // declaratively construct a build graph that will be executed by an external
@@ -26,8 +27,22 @@ pub fn build(b: *std.Build) void {
     exe.linkLibC();
     exe.linkSystemLibrary("SDL2");
     exe.linkSystemLibrary("webp");
-    exe.addIncludePath("/opt/homebrew/include");
-    exe.addLibraryPath("/opt/homebrew/lib");
+    switch (builtin.os.tag) {
+        .macos => {
+            switch (builtin.cpu.arch) {
+                .aarch64 => {
+                    exe.addIncludePath("/opt/homebrew/include");
+                    exe.addLibraryPath("/opt/homebrew/lib");
+                },
+                .x86_64 => {
+                    exe.addIncludePath("/usr/local/include");
+                    exe.addLibraryPath("/usr/local/lib");
+                },
+                else => {},
+            }
+        },
+        else => {},
+    }
 
     // This declares intent for the executable to be installed into the
     // standard location when the user invokes the "install" step (the default
