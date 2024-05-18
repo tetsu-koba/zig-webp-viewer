c3fb1e64bfa2f4f5c9a058fc5fc47dbad7516cb3
Tetsuyuki Kobayashi <tetu.koba@gmail.com>
2024-05-18 17:28:05 +0900

    Update build.zig with zig init

--- build.zig
+++ build.zig
@@ -16,11 +16,23 @@ pub fn build(b: *std.Build) void {
     // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
 
-    const exe = b.addExecutable(.{
-        .name = "webp-viewer",
+    const lib = b.addStaticLibrary(.{
+        .name = "zig-webp-viewer",
         // In this case the main source file is merely a path, however, in more
         // complicated build scripts, this could be a generated file.
-        .root_source_file = .{ .path = "src/main.zig" },
+        .root_source_file = b.path("src/root.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    // This declares intent for the library to be installed into the standard
+    // location when the user invokes the "install" step (the default step when
+    // running `zig build`).
+    b.installArtifact(lib);
+
+    const exe = b.addExecutable(.{
+        .name = "zig-webp-viewer",
+        .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
@@ -31,12 +43,12 @@ pub fn build(b: *std.Build) void {
         .macos => {
             switch (builtin.cpu.arch) {
                 .aarch64 => {
-                    exe.addIncludePath(.{ .path = "/opt/homebrew/include" });
-                    exe.addLibraryPath(.{ .path = "/opt/homebrew/lib" });
+                    exe.addIncludePath(.{ .cwd_relative = "/opt/homebrew/include" });
+                    exe.addLibraryPath(.{ .cwd_relative = "/opt/homebrew/lib" });
                 },
                 .x86_64 => {
-                    exe.addIncludePath(.{ .path = "/usr/local/include" });
-                    exe.addLibraryPath(.{ .path = "/usr/local/lib" });
+                    exe.addIncludePath(.{ .cwd_relative = "/usr/local/include" });
+                    exe.addLibraryPath(.{ .cwd_relative = "/usr/local/lib" });
                 },
                 else => {},
             }
@@ -74,17 +86,26 @@ pub fn build(b: *std.Build) void {
 
     // Creates a step for unit testing. This only builds the test executable
     // but does not run it.
-    const unit_tests = b.addTest(.{
-        .root_source_file = .{ .path = "src/main.zig" },
+    const lib_unit_tests = b.addTest(.{
+        .root_source_file = b.path("src/root.zig"),
         .target = target,
         .optimize = optimize,
     });
 
-    const run_unit_tests = b.addRunArtifact(unit_tests);
+    const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
+
+    const exe_unit_tests = b.addTest(.{
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
 
     // Similar to creating the run step earlier, this exposes a `test` step to
     // the `zig build --help` menu, providing a way for the user to request
     // running the unit tests.
     const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_unit_tests.step);
+    test_step.dependOn(&run_lib_unit_tests.step);
+    test_step.dependOn(&run_exe_unit_tests.step);
 }
